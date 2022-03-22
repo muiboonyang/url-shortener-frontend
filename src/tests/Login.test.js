576c5076b68565c2764d5fb81918d3c16a9f8b23
Ernest <muiboonyang@gmail.com>
2022-03-23 02:28:12 +0800

    Comment out tests

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,15 +1,15 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import Login from "../pages/Login";
-import { shallow, configure } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+// import { render, screen, fireEvent } from "@testing-library/react";
+// import Login from "../pages/Login";
+// import { shallow, configure } from "enzyme";
+// import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
-describe("Test form submission", () => {
-  test("Submits", () => {
-    const onSubmit = jest.fn();
-    const wrapper = shallow(<Login onSubmit={onSubmit} />);
-    wrapper.find("form").simulate("submit");
-    expect(onSubmit).toHaveBeenCalled();
-  });
-});
+// describe("Test form submission", () => {
+//   test("Submits", () => {
+//     const onSubmit = jest.fn();
+//     const wrapper = shallow(<Login onSubmit={onSubmit} />);
+//     wrapper.find("form").simulate("submit");
+//     expect(onSubmit).toHaveBeenCalled();
+//   });
+// });
--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,13 +1,13 @@
-import { render } from "@testing-library/react";
-import Profile from "../pages/Profile";
+// import { render } from "@testing-library/react";
+// import Profile from "../pages/Profile";
 
-describe("async component", () => {
-  test("renders User Profile if profile fetch request succeeds", async () => {
-    window.fetch = jest.fn();
-    window.fetch.mockResolvedValueOnce({
-      json: async () => [{ username: "test", password: "test" }],
-    });
+// describe("async component", () => {
+//   test("renders User Profile if profile fetch request succeeds", async () => {
+//     window.fetch = jest.fn();
+//     window.fetch.mockResolvedValueOnce({
+//       json: async () => [{ username: "test", password: "test" }],
+//     });
 
-    render(<Profile />);
-  });
-});
+//     render(<Profile />);
+//   });
+// });
