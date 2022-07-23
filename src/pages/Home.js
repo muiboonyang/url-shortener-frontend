1c1194a8537441fef66c2a5fc90f116d53a1315d
Ernest <muiboonyang@gmail.com>
2022-07-24 03:21:49 +0800

    Completed implementation of redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,48 @@
 import React from "react";
 import HomepageCarousel from "../components/HomepageCarousel";
 
-import styles from "./Home.module.css";
-import Button from "@mui/material/Button";
+// import styles from "./Home.module.css";
+// import Button from "@mui/material/Button";
+// import Stack from "@mui/material/Stack";
 
 const Home = () => {
-  const seedUser = async () => {
-    try {
-      await fetch(`https://url-shortener-sg.herokuapp.com/seeduser`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const seedUser = async () => {
+  //   try {
+  //     await fetch(`https://url-shortener-sg.herokuapp.com/seeduser`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
-  const deleteUsers = async () => {
-    try {
-      await fetch(`https://url-shortener-sg.herokuapp.com/delete/alluser`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const deleteUsers = async () => {
+  //   try {
+  //     await fetch(`https://url-shortener-sg.herokuapp.com/delete/alluser`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   return (
     <>
       <HomepageCarousel />
-      <div className={styles.buttons}>
-        <Button variant="outlined" color="success" onClick={seedUser}>
+      {/* <div className={styles.container}>
+        <Button variant="contained" color="success" onClick={seedUser}>
           Seed Users
         </Button>
-        <Button variant="outlined" color="error" onClick={deleteUsers}>
+        <Button variant="contained" color="error" onClick={deleteUsers}>
           Delete Users
         </Button>
-      </div>
+      </div> */}
     </>
   );
 };
