367b6d8ce9225f8e3607b95b42c41d8075bd506f
Ernest <muiboonyang@gmail.com>
2022-07-30 01:40:05 +0800

    Replaced carrousel with responsive background images

--- src/components/HomepageCarousel.tsx
+++ src/components/HomepageCarousel.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./HomepageCarousel.module.css";
-import Carousel from "react-bootstrap/Carousel";
+// import styles from "./HomepageCarousel.module.css";
+// import Carousel from "react-bootstrap/Carousel";
+// import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
 
 import { RootState } from "../redux/store";
 import { useSelector } from "react-redux";
+
+const Root = styled("div")(({ theme }) => ({
+  padding: theme.spacing(45),
+  [theme.breakpoints.down("md")]: {
+    backgroundImage:
+      "url('https://i.picsum.photos/id/1053/3596/2393.jpg?hmac=shru06Q5TiVeHArvSrQdBwclhorTJwYGnprqLqaH0hk')",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    height: "100%",
+  },
+  [theme.breakpoints.up("md")]: {
+    backgroundImage:
+      "url('https://i.picsum.photos/id/1043/5184/3456.jpg?hmac=wsz2e0aFKEI0ij7mauIr2nFz2pzC8xNlgDHWHYi9qbc')",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    height: "100%",
+  },
+  [theme.breakpoints.up("lg")]: {
+    backgroundImage:
+      "url('https://i.picsum.photos/id/1036/4608/3072.jpg?hmac=Tn9CS_V7lFSMMgAI5k1M38Mdj-YEJR9dPJCyeXNpnZc')",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+    height: "100%",
+  },
+}));
 
 const HomepageCarousel = () => {
   const username = useSelector((state: RootState) => state.user.username);
@@ -13,53 +40,11 @@ const HomepageCarousel = () => {
     <>
       {username ? (
         <Link to="/myurls">
-          <Carousel
-            className={styles.carousel}
-            indicators={false}
-            controls={false}
-            pause={false}
-          >
-            <Carousel.Item interval={5000} className={styles.fillscreen}>
-              <img
-                className="d-block w-100"
-                src="https://i.picsum.photos/id/1033/2048/1365.jpg?hmac=zEuPfX7t6U866nzXjWF41bf-uxkKOnf1dDrHXmhcK-Q"
-                alt="carousel-1"
-              />
-            </Carousel.Item>
-
-            <Carousel.Item interval={5000} className={styles.fillscreen}>
-              <img
-                className="d-block w-100"
-                src="https://i.picsum.photos/id/1041/5184/2916.jpg?hmac=TW_9o6HeD7H7I7NVo-S1Fa1iAvzQ10uvmJqsXvNoi0M"
-                alt="carousel-2"
-              />
-            </Carousel.Item>
-          </Carousel>
+          <Root />
         </Link>
       ) : (
         <Link to="/register">
-          <Carousel
-            className={styles.carousel}
-            indicators={false}
-            controls={false}
-            pause={false}
-          >
-            <Carousel.Item interval={5000} className={styles.fillscreen}>
-              <img
-                className="d-block w-100"
-                src="https://i.picsum.photos/id/1033/2048/1365.jpg?hmac=zEuPfX7t6U866nzXjWF41bf-uxkKOnf1dDrHXmhcK-Q"
-                alt="carousel-1"
-              />
-            </Carousel.Item>
-
-            <Carousel.Item interval={5000} className={styles.fillscreen}>
-              <img
-                className="d-block w-100"
-                src="https://i.picsum.photos/id/1041/5184/2916.jpg?hmac=TW_9o6HeD7H7I7NVo-S1Fa1iAvzQ10uvmJqsXvNoi0M"
-                alt="carousel-2"
-              />
-            </Carousel.Item>
-          </Carousel>
+          <Root />
         </Link>
       )}
     </>
